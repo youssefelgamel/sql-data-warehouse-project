53ce016dc9849f074856aedecc9bea7a11064b8e
Youssef Ramadan Elgamal <104696508+youssefelgamel@users.noreply.github.com>
2026-02-20 15:19:50 +0200

    Load data into silver.crm_prd_info table

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -72,3 +72,44 @@ BEGIN
 		SET @end_time = GETDATE();
         PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
         PRINT '>> -------------';
+
+
+
+		-- Loading silver.crm_prd_info
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: silver.crm_prd_info';
+		TRUNCATE TABLE silver.crm_prd_info;
+		PRINT '>> Inserting Data Into: silver.crm_prd_info';
+		INSERT INTO silver.crm_prd_info (
+		    prd_id,
+		    cat_id,
+		    prd_key,
+		    prd_nm,
+		    prd_cost,
+		    prd_line,
+		    prd_start_dt,
+		    prd_end_dt
+		)
+		SELECT
+		    prd_id,
+		    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+		    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+		    prd_nm,
+		    ISNULL(prd_cost, 0) AS prd_cost,
+		    CASE UPPER(TRIM(prd_line))
+		        WHEN 'M' THEN 'Mountain'
+		        WHEN 'R' THEN 'Road'
+		        WHEN 'S' THEN 'Other Sales'
+		        WHEN 'T' THEN 'Touring'
+		        ELSE 'n/a'
+		    END AS prd_line,
+		    CAST(prd_start_dt AS DATE) AS prd_start_dt,
+		    DATEADD(
+		        DAY, 
+		        -1, 
+		        LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)
+		    ) AS prd_end_dt
+		FROM bronze.crm_prd_info;
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+		PRINT '>> -------------';
